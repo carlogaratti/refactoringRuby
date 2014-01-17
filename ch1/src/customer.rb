59b8cda7d77d548e0405c2649da7ef2580f3a88c
Carlo Garatti <carlo.garatti@neomobile.com>
2014-01-17 09:42:02 +0100

    applied Replace Temps with Query for totalCharge

--- ch1/src/customer.rb
+++ ch1/src/customer.rb
@@ -11,18 +11,20 @@ class Customer
     end
 
     def statement
-        total_amount, frequent_renter_points = 0,0 
+        frequent_renter_points = 0 
         result = "Rental Record for #{@name} "
         @rentals.each do |aRental|
             result += aRental.movie.title + " " 
-            total_amount +=  aRental.charge
             frequent_renter_points += aRental.freq_renter_points
         end
 
-        result += "Amount owned is #{total_amount} "
+        result += "Amount owned is #{total_charge} "
         result += "You earned #{frequent_renter_points} frequent renter points"
         result
     end
 
+    def total_charge
+        @rentals.inject(0) { |sum, rental| sum + rental.charge }
+    end
 end
 
