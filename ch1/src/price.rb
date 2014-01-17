9c36c049c66eab666d6d43850af0aa35c3ff16b3
Carlo Garatti <carlo.garatti@neomobile.com>
2014-01-17 11:13:00 +0100

    Extract Module for frequent enter points on the prices classes

--- ch1/src/price.rb
+++ ch1/src/price.rb
@@ -1,4 +1,14 @@
+
+module Price
+    def frequent_renter_points(days_rented)
+        1
+    end
+end
+
 class RegularPrice
+   
+    include Price
+
     def charge(days_rented)
         result = 2
         result += (days_rented - 2) * 1.5 if days_rented > 2
@@ -10,9 +20,16 @@ class NewReleasePrice
     def charge(days_rented)
         2
     end
+
+    def frequent_renter_points(days_rented)
+        2
+    end
 end
 
 class ChildrenPrice
+
+    include Price
+
     def charge(days_rented)
         result = 1.5
         result += (days_rented - 3) * 1.5 if days_rented > 3
