6f0dfe335ec7d1696e6958888091542a20af2242
Carlo Garatti <carlo.garatti@neomobile.com>
2014-01-17 08:57:17 +0100

    move method charge from customer to rental

--- ch1/src/customer.rb
+++ ch1/src/customer.rb
@@ -14,7 +14,7 @@ class Customer
         total_amount, frequent_renter_points = 0,0 
         result = "Rental Record for #{@name} "
         @rentals.each do |element|
-            this_amount = amount_for(element)
+            this_amount = element.charge
             frequent_renter_points += 1
             if element.movie.price_code == Movie::NEW_RELEASE && element.days_rented > 1
                 frequent_renter_points += 1
@@ -28,18 +28,4 @@ class Customer
         result
     end
 
-    def amount_for(rental)
-            this_amount = 0
-            case rental.movie.price_code
-            when Movie::REGULAR
-                this_amount += 2
-                this_amount += (rental.days_rented - 2) * 1.5 if rental.days_rented > 2
-            when Movie::NEW_RELEASE
-                this_amount += 2
-            when Movie::CHILDRENS
-                this_amount += 1.5
-                this_amount += (rental.days_rented - 3) * 1.5 if rental.days_rented > 3
-            end
-            this_amount
-    end
 end
