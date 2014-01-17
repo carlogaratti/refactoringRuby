c4285326644a7afa3000518ca98b1fc3c58ba78f
Carlo Garatti <carlo.garatti@neomobile.com>
2014-01-17 10:59:24 +0100

    Self encapsulate Field on price_code and new classes for price_code behavior

--- ch1/src/movie.rb
+++ ch1/src/movie.rb
@@ -1,26 +1,40 @@
+
+require File.expand_path(File.dirname(__FILE__)) + '/../src/price'
+
 class Movie
     REGULAR = 0
     NEW_RELEASE = 1
     CHILDRENS = 2
 
     attr_reader :title
-    attr_accessor :price_code
+    attr_reader :price_code
 
     def initialize(title, price_code)
-        @title, @price_code = title, price_code
+        @title, self.price_code = title, price_code
+    end
+    
+    def price_code=(value)
+        @price_code = value
+        @price = case price_code
+                 when REGULAR 
+                     RegularPrice.new
+                 when NEW_RELEASE 
+                     NewReleasePrice.new
+                 when CHILDRENS 
+                     ChildrenPrice.new
+                 end
     end
 
+    
     def charge(days_rented)
             this_amount = 0
             case price_code
             when REGULAR
-                this_amount += 2
-                this_amount += (days_rented - 2) * 1.5 if days_rented > 2
+               return @price.charge(days_rented)
             when NEW_RELEASE
-                this_amount += 2
+               return @price.charge(days_rented)
             when CHILDRENS
-                this_amount += 1.5
-                this_amount += (days_rented - 3) * 1.5 if days_rented > 3
+               return @price.charge(days_rented)
             end
             this_amount
     end
